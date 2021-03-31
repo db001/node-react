430717e98b87c98e24f51bd530aeab14b5234b4a
Dave <db001@users.noreply.github.com>
2021-03-31 16:22:24 +0100

    Before Stripe

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: "center" }}>
+			<h1>DM Helper</h1>
+			Your D&amp;D Dashboard
+		</div>
+	);
+};
+export default Landing;
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,13 +1,20 @@
 import { FETCH_USER } from "../actions/types";
 
 const authReducer = (state = null, action) => {
-	console.log(action);
 	switch (action.type) {
 		case FETCH_USER:
-			return action.payload || false;
+			// return action.payload || false;
+			return isEmpty(action.payload) ? false : action.payload;
 		default:
 			return state;
 	}
 };
 
 export default authReducer;
+
+function isEmpty(obj) {
+	for (let key in obj) {
+		if (obj.hasOwnProperty(key)) return false;
+	}
+	return true;
+}
