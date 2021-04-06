2149a202a92a43f359b8d2a7a4243ef33edf4a4b
Dave <db001@users.noreply.github.com>
2021-04-06 17:09:19 +0100

    Login required middleware added

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,16 +1,22 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripe.secret);
-
 const pool = require("../database/db");
+const requireLogin = require("../middlewares/requireLogins");
 
 module.exports = (app) => {
-	app.post("/api/stripe", async (req, res) => {
+	app.post("/api/stripe", requireLogin, async (req, res) => {
 		const charge = await stripe.charges.create({
 			amount: 500,
 			currency: "usd",
 			description: "$5 for 5 credits",
 			source: req.body.id,
 		});
+
+		console.log(charge);
+
+		if (charge.status != "succeeded") {
+			return res.status(422).send({ error: "Payment failed" });
+		}
 
 		req.user.credits += 5;
 
@@ -19,6 +25,6 @@ module.exports = (app) => {
 			[req.user.credits, req.user.user_id]
 		);
 
-		console.log(updatedUser.rows[0]);
+		res.send(updatedUser.rows[0]);
 	});
 };
